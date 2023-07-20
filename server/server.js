24ed1531ccbf793eaccf30961678d66b6a59fced
Vita Fomin <vita.bukhalo@gmail.com>
2023-07-20 00:20:17 -0400

    added code and commented out in server.js file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+
+////// Brining in the ApolloServer///// added
+const { ApolloServer } = require("apollo-server-express")
+
 const path = require('path');
+
+//////////// added 
+const { typeDefs, resolvers } = require("./schemas")
+
 const db = require('./config/connection');
-const routes = require('./routes');
+
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//////////////added
+const server = new ApolloServer({ typeDefs, resolvers });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +26,30 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+//
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..client/build/index.html"));
 });
+
+// Creating a new instance of an Apollo server////////added
+const startApolloServer = async () => {
+  await Server.start();
+  Server.applyMiddleware({ app });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`Server up and running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
+};
+
+/////////////// added
+startApolloServer()
+
+
+// app.use(routes);
+
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
