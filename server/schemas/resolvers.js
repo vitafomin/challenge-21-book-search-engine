aeb32db28197d153c02d7a1bc98bd677c92c3e2d
Vita Fomin <vita.bukhalo@gmail.com>
2023-07-21 20:15:35 -0400

    added and updated resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,23 +2,48 @@ const { User, Book } = require("../models");
 
 const resolvers = {
     Query : {
-        user: async (parent, { _id }) => {
-            const params = _id ? { _id } : {};
-            return User.find(params);
+        user: async (parent, { userId }) => {
+            return User.findOne({ _id: userId });
         },
-        books: async () => {
-            return Book.find({});
-        },
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id });
+            }
+            throw new AuthenticationError("Please Login!");
+        }
     },
     mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne ({ email });
+      
+            if (!user) {
+              throw new AuthenticationError("Incorrect email or password");
+            }
+            const correctPass = await user.isCorrectPassword(password);
+      
+            if (!correctPass) {
+              throw new AuthenticationError("Incorrect email or password");
+            }
+      
+            const token = signToken(user);
+            return { token, user }
+          },
         createUser: async (parent, args) => {
             const user = await User.create(args);
             return user;
         },
-        updateUser: async (parent, { id, savedBooks }) => {
-            return await User.findOneAndUpdate({ _id: id }, { savedBooks }, { new: true });
+        saveBook: async (parent, { userId, book }, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate({ _id: userId }, { $addToSet: { books: book }}, { new: true });
+            }
+            throw new AuthenticationError("Please Login!");
+        },
+        removeBook: async (parent, { book }, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate( {_id: context.user._id }, { $pull: {books: book }}, { new: true });
+            }
+            throw new AuthenticationError("Please Login!")
         }
-            
     }
 };
 
